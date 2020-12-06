ea6b6d51f457e95b012a85f385005c932529c716
Dzmitry M <ma.dzmitry@gmail.com>
2020-12-06 15:10:46 +0300

    Initial Commit

---
+++ UberProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UberProject
+//
+//  Created by Dzmitry Matsiulka on 12/6/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
